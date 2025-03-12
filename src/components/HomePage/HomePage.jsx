58b1791b8dce827744f98cfcf827cdf2a1cc62a7
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-03-12 14:47:41 +0200

    installed React Slick

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <>
+    <div className='container'>
+
+    <div>HomePage</div>
+    </div>
+    </>
+  )
+}
+
+export default HomePage
