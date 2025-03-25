fb77605497ec134e9142305ce32a3b1ed212fbb2
Malik <malikhussein229@gmail.com>
2025-03-26 00:42:30 +0200

    Made the fetching user in the page so if no user is found no listing is shown

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import ProfileDetails from '../ProfileDetails/ProfileDetails';
-import ListingCard from '../ListingCard/ListingCard';
 import Listings from '../Listings/Listings';
 import EditProdileModal from '../EditProdileModal/EditProdileModal';
+import { useParams } from 'react-router-dom';
+import useAuthStore from '../../Store/Auth';
+import useProfileStore from '../../Store/profile';
+import { useEffect } from 'react';
 
 export default function ProfilePage() {
-  return (
+  const { id: userId } = useParams();
+  const { token } = useAuthStore();
+  const { profile, loading, errorStatus, fetchProfile} = useProfileStore();
+
+  useEffect(() => {
+    fetchProfile(userId, token);
+  }, [userId, fetchProfile, token]);
+
+  if (loading) return <div>Loading...</div>;
+  if (errorStatus === 500) return <div>Server Error</div>;
+  if (errorStatus === 404) return <div>No User Found</div>;
+
+  if (profile) return (
     <>
-      <ProfileDetails />
+      <ProfileDetails profile={profile} token={token} />
       <Listings />
-      <EditProdileModal />
+      <EditProdileModal profile={profile}/>
     </>
   );
 }
