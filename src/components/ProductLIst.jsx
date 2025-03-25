e31b704bc75dbc101193952fc967e42ad3721a7e
Mohamedeid602 <eidmedo215@gmail.com>
2025-03-25 23:24:24 +0200

    call api for product page

--- src/components/ProductLIst.jsx
+++ src/components/ProductLIst.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import ProductCard from './ProductCard'
+import React, { useEffect, useState } from "react";
+import ProductCard from "./ProductCard";
+import axios from "axios";
 
 export default function ProductLIst() {
+  const [products, setProducts] = useState([]);
+
+  const getallProducts =async () => {
+    axios.get("http://localhost:3000/api/product").then((products) => {
+      console.log(products.data);
+      setProducts(products.data);
+    });
+  };
+
+  useEffect(() => {
+    getallProducts();
+  }, []);
+
   return (
     <>
-             
-   
-          <div className="col-md-9">
-
-              <ProductCard/>     
-              <ProductCard/>     
-              <ProductCard/>     
-              <ProductCard/>     
-
-          </div>      
- 
-  
-    
+      <div className="col-md-9">
+        {products.map((product) => {
+          return <ProductCard product={product} />;
+        })}
+      </div>
     </>
-  )
+  );
 }
