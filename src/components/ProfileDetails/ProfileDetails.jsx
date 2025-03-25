da2f84086441bfac0e73d820c709c89f5e9208d1
Malik <malikhussein229@gmail.com>
2025-03-26 00:43:04 +0200

    Made the deatils dynamic

--- src/components/ProfileDetails/ProfileDetails.jsx
+++ src/components/ProfileDetails/ProfileDetails.jsx
@@ -1,31 +1,36 @@
 import React from 'react';
-import users from '../../assets/users.json';
 import './ProfileDetails.css';
+import useProfileStore from '../../Store/profile';
 
-export default function ProfileDetails() {
-  const user = users[0];
-  console.log(user);
+const ProfileDetails = ({ profile, token}) => {
+  
+  const { isEditable} = useProfileStore();
+
   return (
     <div className="container">
       <div className="d-flex flex-column flex-lg-row justify-content-between align-items-center">
         <div className="d-flex flex-column flex-lg-row">
-          <img src={user.personal_pic} alt="User Picture" width={200} />
+          <img src={profile.personal_pic || 'https://static.vecteezy.com/system/resources/previews/026/619/142/non_2x/default-avatar-profile-icon-of-social-media-user-photo-image-vector.jpg'} alt="User Picture" width={200} />
           <div className="d-flex flex-column justify-content-center align-items-center align-items-lg-start mb-4 mb-lg-0">
-            <h2 className="fw-bold">{user.name}</h2>
-            <h5>{user.email}</h5>
+            <h2 className="fw-bold">{profile.userName}</h2>
+            <h5>{profile.email}</h5>
           </div>
         </div>
-        <div className="d-flex flex-column justify-content-center">
-          <button
-            type="button"
-            data-bs-toggle="modal"
-            data-bs-target="#exampleModal"
-            className="btn btn-primary btn-blue rounded-1 fw-bold py-2 px-4"
-          >
-            Edit Profile
-          </button>
-        </div>
+        {isEditable(token) && (
+          <div className="d-flex flex-column justify-content-center">
+            <button
+              type="button"
+              data-bs-toggle="modal"
+              data-bs-target="#profileModal"
+              className="btn btn-primary btn-blue rounded-1 fw-bold py-2 px-4"
+            >
+              Edit Profile
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
-}
+};
+
+export default ProfileDetails;
