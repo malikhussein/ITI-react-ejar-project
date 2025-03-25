bda71a8cb10e078e9464728e666e8e2504220598
Malik <malikhussein229@gmail.com>
2025-03-26 00:39:38 +0200

    Fetch the listings from the api

--- src/components/ListingCard/ListingCard.jsx
+++ src/components/ListingCard/ListingCard.jsx
@@ -3,30 +3,30 @@ import './ListingCard.css';
 import { Link } from 'react-router-dom';
 
 export default function ListingCard({ product }) {
-  console.log(product);
+  // console.log(product);
   return (
     <div className="col-12 col-sm-6 col-md-4 col-lg-3">
       <div className="card border-0">
-        <a className="square-image position-relative d-block">
+        <div className="square-image position-relative d-block">
           <Link to="/product/123">
-            <img src={product.image} alt="" />
+            <img src={product.images[0]} alt="" />
           </Link>
           <h5 onClick={() => console.log('working')}>
-            <span className="position-absolute top-0 start-0 badge rounded-pill bg-primary">
-              Primary
+            <span class="position-absolute top-0 start-0 badge rounded-pill bg-success">
+              {product.status}
             </span>
           </h5>
-        </a>
+        </div>
         <div className="card-body">
           <Link
             className="card-title text-nowrap overflow-hidden fw-bold h4 text-center text-decoration-none d-block"
             to="/product/123"
           >
-            {product.title}
+            {product.name}
           </Link>
           <div className="d-flex flex-column justify-content-between">
             <p className="card-title h5 mt-1">Listed on 10/2/2025</p>
-            <p className="card-title h5 mt-1">{product.price}/day</p>
+            <p className="card-title h5 mt-1">{product.price.daily}/day</p>
           </div>
         </div>
       </div>
--- src/components/Listings/Listings.jsx
+++ src/components/Listings/Listings.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ListingCard from '../ListingCard/ListingCard';
 import products from '../../assets/products.json';
+import useProductStore from '../../Store/product';
+import useAuthStore from '../../Store/Auth';
+import { useParams } from 'react-router-dom';
 
 export default function Listings() {
-  console.log(products);
+  const { id: userId } = useParams();
+  const { userProducts, getUserProducts } = useProductStore();
+  const { token } = useAuthStore();
+
+  useEffect(() => {
+    getUserProducts(token, userId);
+  }, [getUserProducts, token, userId]);
+
   return (
     <div className="container">
       <h2 className="my-5 border-bottom pb-3 text-center text-lg-start">
         Listings
       </h2>
       <div className="row gx-5 mx-5">
-        {products.map((product) => (
-          <ListingCard key={product.id} product={product} />
-        ))}
+        {userProducts.length === 0 ? (
+          <h3 className="text-center mb-5 h1">No Listings Found</h3>
+        ) : (
+          userProducts.map((product) => (
+            <ListingCard key={product.id} product={product} />
+          ))
+        )}
       </div>
     </div>
   );
