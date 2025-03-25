508ba695fb9329ab22f0401f4698c78a0b48b023
Malik <malikhussein229@gmail.com>
2025-03-26 00:35:39 +0200

    Made all the api use port 3000

--- src/Store/Deatils.js
+++ src/Store/Deatils.js
@@ -1,69 +1,58 @@
-import { create } from "zustand";
-import axios from "axios";
+import { create } from 'zustand';
+import axios from 'axios';
 
 const useProductStore = create((set) => ({
   product: null,
   productList: [],
   fetchProduct: async (id) => {
     try {
-      const response = await axios.get(`http://localhost:4200/api/product/${id}`);
+      const response = await axios.get(
+        `http://localhost:3000/api/product/${id}`
+      );
 
       set({ product: response.data });
     } catch (error) {
-      console.error("Error fetching product:", error);
+      console.error('Error fetching product:', error);
     }
   },
 
-  
   updateProduct: async (updatedData) => {
     try {
-      
-      
       const response = await axios.post(
-        `http://localhost:4200/api/product/${updatedData._id}`,
+        `http://localhost:3000/api/product/${updatedData._id}`,
         updatedData
       );
-  
-  
+
       set((state) => ({
         product: {
           ...state.product,
           data: {
-            ...state.product.data, 
-            ...updatedData, 
+            ...state.product.data,
+            ...updatedData,
           },
         },
       }));
     } catch (error) {
-      console.error("Error updating product:", error.response?.data || error.message);
+      console.error(
+        'Error updating product:',
+        error.response?.data || error.message
+      );
     }
   },
   getAllProd: async (id) => {
     try {
       console.log('got');
-      
-      const response = await axios.get(`http://localhost:4200/api/product/?category=${id}`);
+
+      const response = await axios.get(
+        `http://localhost:3000/api/product/?category=${id}`
+      );
       console.log(response);
-      
-      set({ productList: response.data.data }); 
+
+      set({ productList: response.data.data });
     } catch (error) {
-      console.error("Error fetching products:", error.message);
+      console.error('Error fetching products:', error.message);
     }
   },
-  
 }));
 
-  
-
-
-
-
-
-export { useProductStore   };
-
-
-
-
-
-
-
+export { useProductStore };
