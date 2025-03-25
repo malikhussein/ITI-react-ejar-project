40c51440500746d1d7b6b41f67823df0907ea8ef
Malik <malikhussein229@gmail.com>
2025-03-26 00:35:48 +0200

    Product Store

---
+++ src/Store/product.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import { create } from 'zustand';
+import { jwtDecode } from 'jwt-decode';
+
+const useProductStore = create((set, get) => ({
+  userProducts: [],
+  error: null,
+  loading: false,
+  postProduct: async (token, productData) => {
+    try {
+      set({ loading: true, error: null });
+      const decoded = jwtDecode(token);
+      const userId = decoded.id;
+      productData.append('renterId', userId);
+      const response = await axios.post(
+        `http://localhost:3000/api/product`,
+        productData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'multipart/form-data'
+          },
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      set({ error: error.response?.data?.message || error.message });
+    }
+  },
+  getUserProducts: async (token, userId) => {
+    try {
+      set({ loading: true, error: null });
+      const response = await axios.get(`http://localhost:3000/api/product`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const allProducts = response?.data?.data;
+      const userProducts = allProducts.filter(
+        (product) => product.renterId === userId
+      );
+      set({ userProducts, loading: false });
+      console.log(allProducts);
+      console.log(userProducts);
+    } catch (error) {
+      set({ error: error.response?.data?.message || error.message });
+    }
+  },
+}));
+
+export default useProductStore;
