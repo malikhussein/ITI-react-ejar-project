07f67e2984e8eedacde0fb864d23e19f00b4e416
Mohamedeid602 <eidmedo215@gmail.com>
2025-03-26 04:19:26 +0200

    add product page query param

--- src/components/ProductLIst.jsx
+++ src/components/ProductLIst.jsx
@@ -1,15 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductCard from './ProductCard';
 
 import useProductStore from '../Store/productsStore';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
+
+// const { products, fetchProducts } = useProductStore();
+
+const useQuery = () => {
+  return new URLSearchParams(useLocation().search);
+};
+
 
 export default function ProductLIst() {
-  const { products, fetchProducts } = useProductStore();
 
+  const [products, setProducts] = useState([]);
+
+  const query = useQuery();
+  const categoryName = query.get("category"); // Get category from query params
+
+  
   useEffect(() => {
-    fetchProducts();
-  }, [fetchProducts]);
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3000/api/product`, {
+          params: { category: categoryName }, // Send category as query param
+        });
 
+        setProducts(response.data.data);
+      } catch (err) {
+        console.log( (err.message));
+      }
+    };
+
+    if (categoryName) {
+      fetchProducts();
+    }
+  }, [categoryName]);
+  
   return (
     <>
       <div className="col-md-9">
