6242a66f4781bb45705926c38f87abe632fa7a69
Malik <malikhussein229@gmail.com>
2025-03-30 16:27:23 +0200

    Added link to product title

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,6 @@
 // import product from "../assets/1.jpg"
 
+import { Link } from 'react-router-dom';
 import './ProductCard.css';
 
 export default function ProductCard({ product }) {
@@ -16,21 +17,25 @@ export default function ProductCard({ product }) {
           id="imgProduct"
           className="col-12 col-md-auto d-flex justify-content-center"
         >
-          <img
-            src={product.images[0]}
-            className="bd-placeholder-img img-fluid"
-            width={300}
-            height={260}
-          />
+          <Link to={`/product/${product._id}`}>
+            <img
+              src={product.images[0]}
+              className="bd-placeholder-img img-fluid"
+              width={300}
+              height={260}
+            />
+          </Link>
         </div>
 
         <div
           id="data"
           className="col-12 col-md-5 p-4 d-flex flex-column position-static text-center text-md-start"
         >
-          <h2 id="title" className="d-inline-block mb-2">
-            {product.name}
-          </h2>
+          <Link to={`/product/${product._id}`}>
+            <h2 id="title" className="d-inline-block mb-2">
+              {product.name}
+            </h2>
+          </Link>
           <h5>Description:</h5>
           <h5>{product.description}</h5>
           <h2 id="price">{product.daily} E.G</h2>
