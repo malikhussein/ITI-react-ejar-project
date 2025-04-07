67f21d9b9bdc2e3ba3c831bf2994ee474fb44758
Malik <malikhussein229@gmail.com>
2025-04-07 20:10:05 +0200

    the procses store that handles all the process api calls and state mangement

---
+++ src/Store/process.js
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import { create } from 'zustand';
+
+const useProcessStore = create((set, get) => ({
+  userProcesses: [],
+  error: null,
+  loading: false,
+  getProcesses: async (token) => {
+    try {
+      set({ loading: true, error: null });
+      const response = await axios.get(`http://localhost:3000/api/process`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const processes = response?.data;
+      set({ userProcesses: processes, loading: false });
+    } catch (error) {
+      set({ error: error.response?.data?.message || error.message });
+    }
+  },
+  postProcess: async (productId, processData, token) => {
+    try {
+      set({ loading: true, error: null });
+      const response = await axios.post(
+        `http://localhost:3000/api/process/${productId}`,
+        processData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    } catch (error) {
+      set({ error: error.response?.data?.message || error.message });
+    }
+  },
+  acceptProcess: async (processId, token) => {
+    try {
+      set({ loading: true, error: null });
+      const response = await axios.put(
+        `http://localhost:3000/api/process/${processId}`,
+        { status: 'in_progress' },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const updatedProcess = response?.data;
+    } catch (error) {
+      set({ error: error.response?.data?.message || error.message });
+    }
+  },
+  declineProcess: async (processId, token) => {
+    try {
+      set({ loading: true, error: null });
+      const response = await axios.put(
+        `http://localhost:3000/api/process/${processId}`,
+        { status: 'canceled' },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const updatedProcess = response?.data;
+    } catch (error) {
+      set({ error: error.response?.data?.message || error.message });
+    }
+  },
+}));
+
+export default useProcessStore;
