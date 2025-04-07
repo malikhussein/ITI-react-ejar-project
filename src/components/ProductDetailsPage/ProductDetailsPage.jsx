6b0b65fc02dc4f9502ca62d91175f12544928f8b
Malik <malikhussein229@gmail.com>
2025-04-07 20:18:54 +0200

    Added the modal to the details page

--- src/components/ProductDetailsPage/ProductDetailsPage.jsx
+++ src/components/ProductDetailsPage/ProductDetailsPage.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import RelatedProducts from "../relatedItem/relatedItem";
-import ProductDetails from "../ProductDetails/productDeatils";
-import Reviews from "../reviews/reviews";
+import React from 'react';
+import RelatedProducts from '../relatedItem/relatedItem';
+import ProductDetails from '../ProductDetails/productDeatils';
+import Reviews from '../reviews/reviews';
+import RentProductModal from '../RentProductModal/RentProductModal';
 
 export default function ProductDetailsPage() {
   return (
     <>
+      <RentProductModal />
       <ProductDetails />
       <Reviews />
       <RelatedProducts />
