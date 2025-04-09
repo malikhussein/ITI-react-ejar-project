b2db9a33e65fb41d3e8b5bec814776be44ea63e1
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-04-10 01:16:19 +0200

    fix section styles

--- src/components/StartRenting/StartRenting.jsx
+++ src/components/StartRenting/StartRenting.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./StartRenting.css";
 
 function StartRenting() {
   return (
     <div
-      className="sart-renting mt-5 d-flex justify-content-between mb-5 flex-wrap gap-4"
+      className="start mt-5 d-flex justify-content-between mb-5 flex-wrap gap-4"
       data-aos="fade-up"
     >
-      <div className="text" data-aos="fade-up">
+      <div className="text">
         <h2 className="rent-head">Turn Your Unused Items Into Earnings!</h2>
         <p className="rent-p mt-4 fs-5">
           Why let your items sit unused when you can make money renting them
