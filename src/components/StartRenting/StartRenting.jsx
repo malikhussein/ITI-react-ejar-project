9af7ca06118892e49272b1609335579fed41d6a8
Malik <malikhussein229@gmail.com>
2025-04-11 00:19:27 +0200

    Added abutton for the guest to navigate him to the register page

--- src/components/StartRenting/StartRenting.jsx
+++ src/components/StartRenting/StartRenting.jsx
@@ -1,19 +1,18 @@
-import React from "react";
-import "./StartRenting.css";
-import useAuthStore from "../../Store/Auth";
+import React from 'react';
+import './StartRenting.css';
+import useAuthStore from '../../Store/Auth';
+import { Link } from 'react-router-dom';
 
 function StartRenting() {
   const { token } = useAuthStore();
 
   return (
-    <div
-      className="start mt-5 d-flex justify-content-between mb-5 flex-wrap gap-4"
-    >
+    <div className="start mt-5 d-flex justify-content-between mb-5 flex-wrap gap-4">
       <div className="text">
         <h2 className="rent-head">Turn Your Unused Items Into Earnings!</h2>
         <p className="rent-p mt-4 fs-5">
           Why let your items sit unused when you can make money renting them
-          out?{" "}
+          out?{' '}
         </p>
         <p className="fs-4 mb-0">Join Ejar today and start earning!</p>
       </div>
@@ -29,7 +28,11 @@ function StartRenting() {
             Start Renting Out
           </button>
         ) : (
-          ""
+          <Link className="mt-auto" to="/register">
+            <button type="button" className="rent-btn">
+              Join Us Now
+            </button>
+          </Link>
         )}
       </div>
     </div>
