215bf1d245c5e52e390d87f00f3ac6b5519c01bf
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-04-11 17:21:41 +0200

    remove unused code and packages

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -4,15 +4,9 @@ import CategoriesSection from '../CategoriesSection/CategoriesSection'
 import './HomePage.css'
 import StartRenting from '../StartRenting/StartRenting'
 import HomeItemsSection from '../HomeItemsSection/HomeItemsSection'
-import Aos from 'aos';
-import "aos/dist/aos.css"
 
 function HomePage() {
 
-  useEffect(() => {
-    Aos.init({duration: 2000})
-  
-  }, [])
   return (
     <>
     <div className='home mt-4'></div>
