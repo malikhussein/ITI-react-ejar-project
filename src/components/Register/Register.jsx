c090646d553acb03aff7f95a3fd079367495d535
Test0-VC <omarabdin1@gmail.com>
2025-04-12 15:38:58 +0200

    .

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -37,7 +37,13 @@ export default function Register() {
       .max(30, "Name should be at most 30 characters long")
       .matches(/^[a-zA-Z_ ]+$/, "Name must only contain letters, underscores, and spaces.")
       .required("Name is required"),
-    email: Yup.string().email("Invalid email address").required("Email is required"),
+      email: Yup.string()
+      .email("Invalid email address")
+      .matches(
+        /^[\w-.]+@([\w-]+\.)+(com|net|org|eg)$/i,
+        "Please enter a valid email ending in .com, .net, .org, or .eg"
+      )
+      .required("Email is required"),    
     password: Yup.string()
     .min(8, "Password should be at least 8 characters long")
     .matches(
