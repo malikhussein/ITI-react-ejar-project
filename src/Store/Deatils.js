c00dba8f2dd7a378a04353573797b64c0b7bc32e
Malik <malikhussein229@gmail.com>
2025-04-13 14:48:29 +0200

    handle the api to take the token in the headers

--- src/Store/Deatils.js
+++ src/Store/Deatils.js
@@ -1,6 +1,6 @@
-import { create } from "zustand";
-import axios from "axios";
-import { toast } from "react-toastify";
+import { create } from 'zustand';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const useProductStore = create((set) => ({
   product: null,
@@ -17,23 +17,22 @@ const useProductStore = create((set) => ({
       const response = await axios.get(
         `http://localhost:3000/api/product/${id}`
       );
-      set({ product: response.data, error: null , loading: false });
+      set({ product: response.data, error: null, loading: false });
     } catch (error) {
-      console.error("Error fetching product:", error);
+      console.error('Error fetching product:', error);
       set({ product: null, error: error.message, loading: false });
-    } 
+    }
   },
 
-  updateProduct: async (updatedData, clearMessage = false) => {
+  updateProduct: async (updatedData, clearMessage = false, token) => {
     try {
       const formData = new FormData();
 
       if (clearMessage) {
-        updatedData.confirmMessage = "";
+        updatedData.confirmMessage = '';
       }
 
       for (const key in updatedData) {
-        
         if (Array.isArray(updatedData[key])) {
           updatedData[key].forEach((item) => {
             formData.append(key, item);
@@ -42,20 +41,20 @@ const useProductStore = create((set) => ({
           formData.append(key, updatedData[key]);
         }
       }
-      
+
       const response = await axios.post(
         `http://localhost:3000/api/product/${updatedData._id}`,
         formData,
         {
           headers: {
-            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'multipart/form-data',
           },
         }
       );
-
     } catch (error) {
       console.error(
-        "Error updating product:",
+        'Error updating product:',
         error.response?.data || error.message
       );
     }
@@ -67,7 +66,7 @@ const useProductStore = create((set) => ({
 
       set({ isLoading: true, err: null });
 
-      const categoryId = typeof id === "object" && id !== null ? id._id : id;
+      const categoryId = typeof id === 'object' && id !== null ? id._id : id;
 
       const response = await axios.get(
         `http://localhost:3000/api/product/?category=${categoryId.toString()}`
@@ -77,18 +76,19 @@ const useProductStore = create((set) => ({
 
       set({
         productList: data,
-        isLoading: false,err: null,
+        isLoading: false,
+        err: null,
       });
     } catch (error) {
-      console.error("Error fetching products:", error.message);
+      console.error('Error fetching products:', error.message);
       set({ isLoading: false, err: error.message });
     }
   },
-  
+
   gteProccesOfProduct: async (id, token) => {
     try {
       if (!id) {
-        console.warn("ID is undefined, waiting for data...");
+        console.warn('ID is undefined, waiting for data...');
         return;
       }
 
@@ -104,7 +104,7 @@ const useProductStore = create((set) => ({
       const data = response.data.data?.[0];
       set({ processData: data || null });
     } catch (error) {
-      console.error("Error fetching products:", error.message);
+      console.error('Error fetching products:', error.message);
       set({ processData: null });
     }
   },
